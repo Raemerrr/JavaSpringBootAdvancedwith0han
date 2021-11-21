7b32c85c542ce016117e9d125d2960c3b0322800
Raemin KANG <foals18@gmail.com>
2021-11-22 07:00:32 +0900

    feat: 예제 만들기
    * 로그 추적기 V1 - 적용

---
+++ advanced/src/main/java/hello/advanced/v1/OrderServiceV1.java
@@ -0,0 +1,36 @@
+/*
+ * OrderService
+ * advanced
+ *
+ * Version 0.1
+ *
+ * Created by 강래민 on 2021-11-21.
+ *
+ * Copyright © 2021 raemerrr. All rights reserved.
+ */
+package hello.advanced.v1;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV1 {
+
+    private final OrderRepositoryV1 orderRepositoryV1;
+    private final HelloTraceV1 trace;
+
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            orderRepositoryV1.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
